0d3ae9a9240cb0de94362ba5527602346a060f9a
Omar Tuffaha <omar_tuffaha@hotmail.com>
2019-10-15 20:03:16 +0200

    fixed naming

--- auto_letsencrypt-namecheap.sh
+++ auto_letsencrypt-namecheap.sh
@@ -2,5 +2,5 @@
 openssl genrsa 4096 > user.key
 openssl rsa -in user.key -pubout > user.pub
 openssl genrsa 4096 > domain.key
-openssl req -new -sha256 -key domain.key -subj "/O=Cake Cheers./C=CA" -config openssl-san.cfg >domain.csr
+openssl req -new -sha256 -key domain.key -subj "/O=XXXXXX/C=XX" -config openssl-san.cfg >domain.csr
 python sign_csr_auto.py -f --public-key user.pub domain.csr > signed.crt
